62d387f1e397fd760e14e02339716b945a5b6e57
Geoffrey Negiar <geoffrey.negiar@polytechnique.edu>
2015-12-31 19:03:58 +0100

    Outline finished, //TODO added.

---
+++ include/model.cpp
@@ -0,0 +1,5 @@
+//
+// Created by geoffrey on 31/12/15.
+//
+
+#include "model.h"
--- include/ransac.h
+++ include/ransac.h
@@ -5,11 +5,14 @@
 #ifndef RANSAC_RANSAC_H
 #define RANSAC_RANSAC_H
 
+#include <opencv2/highgui/highgui.hpp>
 #include <vector>
 #include <random>
+#include "model.h"
+
 //TODO: add OpenCV lib use to process images and points (for the modelling)
 
-template<typename dataType>
+template<class dataType>
 class RANSAC {
 private:
     std::vector<dataType> *data; //pointer to the data we want to get a model for
@@ -17,30 +20,55 @@ private:
     double probability; //probability for drawing an outlier free subset
     int minS; //minimal number of data element to correctly estimate the model
     double threshold; //Threshold which defines if a data element, di, agrees with model M.
+    Model model;
 
 public:
     RANSAC<dataType>(std::vector<dataType> *data, double probability, int minS, double threshold) :
             data(data), probability(probability), minS(minS) {
         this->data = data;
         size = data->size();
-        estimateModel(); //we estimate the model on constructions
+        estimateModel(); //we update the object's model using this
+    }
+
+//    TODO: implement this : look at the distance between dataPiece
+//    TODO: and the model, and validate if its under the threshold
+    static bool agree(const Model model, const dataType dataPiece, double threshold) {
+
     }
 
     // main function of the algorithm
-    // TODO: implement this
+    // TODO: finish implementation of this
     void estimateModel() {
-        std::vector<dataType> consensusSet;
+        std::vector<dataType> maximalConsensusSet;
         double N = binomial(minS, size);
-
+        // iterate N times :
         for (int i = 0; i < N; i++) {
-
+            std::vector<dataType> consensusSet;
+            std::vector<dataType> sample = getRandomSample(*data, minS);
+//          TODO: implement this
+//          get the model from the random sample
+            Model curModel = estimateModelFromSample(consensusSet);
+            for (int i = 0; i < size; i++) {
+                if (agree(curModel, data[i], threshold)) {
+                    consensusSet.push_back(data[i]);
+                }
+            }
+            if (consensusSet.size() > maximalConsensusSet.size()) {
+                maximalConsensusSet = consensusSet;
+//              update N
+                N = log(1 - probability) / log(1 - pow(consensusSet.size() / (double) size, minS));
+            }
         }
-
+        model = estimateModelFromSample(maximalConsensusSet);
     }
 
     //given a good sample, this function estimates a model, [both linear and circular, then chooses the best]
-    // TODO: choose data structure for the model. The model is represented by a Matrix in both cases
-    void estimateModelFromSample(dataType *maxConsensusDataSample) { }
+    // TODO: Implement the Model class (and use inheritance for circle/linear models ?)
+    // TODO: (circle and linear). Test this separately also.
+    Model estimateModelFromSample(const std::vector<dataType> &consensusDataSample) const {
+        Model result;
+        return result;
+    }
 
     //simple efficent binomal coefficient function
     static double binomial(int k, int n) {
@@ -53,7 +81,7 @@ public:
 
     // sampling with remplacement.
     //TODO: sample with removal and not replacement
-    static std::vector<dataType> getRandomSample(std::vector<dataType> &data, int sampleSize) {
+    static std::vector<dataType> getRandomSample(const std::vector<dataType> &data, int sampleSize) {
         std::vector<dataType> result;
         if (data.empty() || sampleSize < 1) return result;
         std::random_device rd;
